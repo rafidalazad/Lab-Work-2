fd5dfb8d75f355210e1e6726de196b1a93bfbe11
rafidalazad <141198586+rafidalazad@users.noreply.github.com>
2024-06-09 03:09:25 +1000

    Update graph.c

--- graph.c
+++ graph.c
@@ -1,68 +1,25 @@
-#define _CRT_SECURE_NO_WARNINGS
-#pragma warning(disable:4996)
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "graph.h"
 
-Graph new_graph(int num_vertices) {
-    Graph G;
-    G.V = num_vertices;
-    G.edges = (EdgeList*)malloc(num_vertices * sizeof(EdgeList));
-
-    for (int i = 0; i < num_vertices; i++) {
-        G.edges[i].head = NULL;
+void add_edge(Graph* self, int from, int to, int w) {
+    EdgeNodePtr newNode = (EdgeNodePtr)malloc(sizeof(struct edgeNode));
+    if (!newNode) {
+        printf("Memory allocation failed for new edge node\n");
+        exit(EXIT_FAILURE);
     }
-
-    return G;
+    newNode->edge.to_vertex = to;
+    newNode->edge.weight = w;
+    newNode->next = self->edges[from].head;
+    self->edges[from].head = newNode;
 }
 
-void add_edge(Graph* G, int from_vertex, int to_vertex, int weight) {
-    EdgeNodePtr new_node = (EdgeNodePtr)malloc(sizeof(struct edgeNode));
-    new_node->edge.to_vertex = to_vertex;
-    new_node->edge.weight = weight;
-    new_node->next = G->edges[from_vertex].head;
-    G->edges[from_vertex].head = new_node;
-}
-
-void calculate_in_degrees(Graph G) {
-    int* in_degrees = (int*)malloc(G.V * sizeof(int));
-    if (in_degrees == NULL) {
-        fprintf(stderr, "Memory allocation failed.\n");
-        return;
-    }
-
-    for (int i = 0; i < G.V; i++) {
-        in_degrees[i] = 0;
-    }
-
-    for (int i = 0; i < G.V; i++) {
-        EdgeNodePtr current = G.edges[i].head;
+void calculate_in_degrees(Graph* self, int* in_degrees) {
+    for (int i = 0; i < self->V; i++) {
+        EdgeNodePtr current = self->edges[i].head;
         while (current != NULL) {
             in_degrees[current->edge.to_vertex]++;
             current = current->next;
         }
     }
-
-    // Print in-degrees
-    printf("In-degrees:\n");
-    for (int i = 0; i < G.V; i++) {
-        printf("Vertex %d: %d\n", i, in_degrees[i]);
-    }
-
-    // Free dynamically allocated memory
-    free(in_degrees);
-}
-
-
-void free_graph(Graph* G) {
-    for (int i = 0; i < G->V; i++) {
-        EdgeNodePtr current = G->edges[i].head;
-        while (current != NULL) {
-            EdgeNodePtr temp = current;
-            current = current->next;
-            free(temp);
-        }
-    }
-    free(G->edges);
 }
